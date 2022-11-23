c275642358abc3c12c98c18efea320b9e0383626
kvc0 <3454741+kvc0@users.noreply.github.com>
2022-11-22 18:19:56 -0800

    add protoc setup to github

---
+++ install_protoc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+set -x
+
+VERSION=21.9
+
+sudo apt-get -y install zip tree
+curl -L https://github.com/protocolbuffers/protobuf/releases/download/v$VERSION/protoc-$VERSION-linux-x86_64.zip -o protoc.zip
+unzip -o protoc.zip -d protoc
+
+sudo mv protoc/include/* /usr/local/include/
+
+tree /usr/local/include/google
+
