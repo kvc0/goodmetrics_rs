6ffa0c299330e84d55d108e31c35edea1522619d
kvc0 <3454741+kvc0@users.noreply.github.com>
2024-01-17 11:30:13 -0800

    experiments with gauge dimensions

--- lib/src/gauge_group.rs
+++ lib/src/gauge_group.rs
@@ -5,37 +5,42 @@ use std::{
 
 use crate::{
     gauge::{self, StatisticSetGauge},
-    pipeline::aggregation::statistic_set::StatisticSet,
+    pipeline::{aggregation::statistic_set::StatisticSet, aggregator::DimensionPosition},
     types::Name,
 };
 
 #[derive(Default)]
 pub struct GaugeGroup {
-    gauges: HashMap<Name, Weak<StatisticSetGauge>>,
+    gauges: HashMap<DimensionPosition, HashMap<Name, Weak<StatisticSetGauge>>>,
 }
 
 impl GaugeGroup {
     /// Create or retrieve a gauge.
     pub fn gauge(&mut self, name: impl Into<Name>) -> Arc<StatisticSetGauge> {
+        self.dimensioned_gauge(name, Default::default())
+    }
+
+    pub fn dimensioned_gauge(&mut self, name: impl Into<Name>, dimensions: DimensionPosition) -> Arc<StatisticSetGauge> {
         let name = name.into();
-        match self.gauges.get(&name) {
+        let mut gauge_position = self.gauges.entry(dimensions).or_default();
+        match gauge_position.get(&name) {
             Some(existing) => match existing.upgrade() {
                 Some(existing) => existing,
                 None => {
                     let gauge: Arc<StatisticSetGauge> = Arc::new(gauge::statistic_set_gauge());
-                    self.gauges.insert(name, Arc::downgrade(&gauge));
+                    gauge_position.insert(name, Arc::downgrade(&gauge));
                     gauge
                 }
             },
             None => {
                 let gauge: Arc<StatisticSetGauge> = Arc::new(gauge::statistic_set_gauge());
-                self.gauges.insert(name, Arc::downgrade(&gauge));
+                gauge_position.insert(name, Arc::downgrade(&gauge));
                 gauge
             }
         }
     }
 
-    pub fn reset(&mut self) -> impl Iterator<Item = (Name, StatisticSet)> + '_ {
+    pub fn reset(&mut self) -> (impl Iterator<Item = DimensionedAggregatedGauges<StatisticSet>> + '_) {
         self.gauges.retain(|_name, gauge| gauge.upgrade().is_some());
 
         self.gauges.iter().filter_map(|(name, possible_gauge)| {
@@ -43,6 +48,17 @@ impl GaugeGroup {
                 .upgrade()
                 .and_then(|gauge| gauge.reset())
                 .map(|statistic_set| (name.to_owned(), statistic_set))
-        })
+        });
+        vec![].into_iter()
     }
+}
+
+pub struct DimensionedAggregatedGauges<MeasurementType> {
+    position: DimensionPosition,
+    gauges: Vec<AggregatedGauge<MeasurementType>>,
+}
+
+pub struct AggregatedGauge<MeasurementType> {
+    pub name: Name,
+    measurement: MeasurementType,
 }
