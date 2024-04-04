5df784472e981f1d56cc08e8b28e5dd2af60e7f7
kvc0 <3454741+kvc0@users.noreply.github.com>
2024-04-04 11:09:57 -0700

    rename library folder and minor version bumps

--- goodmetrics/src/proto/opentelemetry.proto.collector.metrics.v1.rs
+++ goodmetrics/src/proto/opentelemetry.proto.collector.metrics.v1.rs
@@ -31,7 +31,7 @@ pub mod metrics_service_client {
         /// Attempt to create a new client by connecting to a given endpoint.
         pub async fn connect<D>(dst: D) -> Result<Self, tonic::transport::Error>
         where
-            D: std::convert::TryInto<tonic::transport::Endpoint>,
+            D: TryInto<tonic::transport::Endpoint>,
             D::Error: Into<StdError>,
         {
             let conn = tonic::transport::Endpoint::new(dst)?.connect().await?;
@@ -87,12 +87,28 @@ pub mod metrics_service_client {
             self.inner = self.inner.accept_compressed(encoding);
             self
         }
+        /// Limits the maximum size of a decoded message.
+        ///
+        /// Default: `4MB`
+        #[must_use]
+        pub fn max_decoding_message_size(mut self, limit: usize) -> Self {
+            self.inner = self.inner.max_decoding_message_size(limit);
+            self
+        }
+        /// Limits the maximum size of an encoded message.
+        ///
+        /// Default: `usize::MAX`
+        #[must_use]
+        pub fn max_encoding_message_size(mut self, limit: usize) -> Self {
+            self.inner = self.inner.max_encoding_message_size(limit);
+            self
+        }
         /// For performance reasons, it is recommended to keep this RPC
         /// alive for the entire life of the application.
         pub async fn export(
             &mut self,
             request: impl tonic::IntoRequest<super::ExportMetricsServiceRequest>,
-        ) -> Result<
+        ) -> std::result::Result<
             tonic::Response<super::ExportMetricsServiceResponse>,
             tonic::Status,
         > {
@@ -109,7 +125,15 @@ pub mod metrics_service_client {
             let path = http::uri::PathAndQuery::from_static(
                 "/opentelemetry.proto.collector.metrics.v1.MetricsService/Export",
             );
-            self.inner.unary(request.into_request(), path, codec).await
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(
+                    GrpcMethod::new(
+                        "opentelemetry.proto.collector.metrics.v1.MetricsService",
+                        "Export",
+                    ),
+                );
+            self.inner.unary(req, path, codec).await
         }
     }
 }
